851468f3cacf492db9bdf2f83268a0432d0708a6
Mustafa Katipoğlu <herolenk@hotmail.com>
2017-08-12 09:59:49 +0300

    add jquery to portfolio, fix general layout

--- code/code.js
+++ code/code.js
@@ -1,10 +1,52 @@
 var main = function(){
+   var currentLink;
     
-    $('#item1').on('click',function(){$('.portf-item').children().removeClass('sPortfolioItem');$('#item1').addClass('sPortfolioItem');});
-    $('#item2').on('click',function(){$('.portf-item').children().removeClass('sPortfolioItem');$('#item2').addClass('sPortfolioItem');});
-    $('#item3').on('click',function(){$('.portf-item').children().removeClass('sPortfolioItem');$('#item3').addClass('sPortfolioItem');});
-    $('#item4').on('click',function(){$('.portf-item').children().removeClass('sPortfolioItem');$('#item4').addClass('sPortfolioItem');});
-    $('#item5').on('click',function(){$('.portf-item').children().removeClass('sPortfolioItem');$('#item5').addClass('sPortfolioItem');});
+    $('.p-item').on('click',function(){
+        
+        var currentBackground = $(this).css('background-image');
+        currentLink = $(this).next('.portLink').attr('href');
+        $('.bigItem').css('background-image', currentBackground);
+        $('#showbig').show();
+        
+        
+    });
+    $('.newWindow').on('click',function(){
+        
+        $('.newWindow').attr('href', currentLink);
+        
+    });
+    $('.b-i-remWindow').on('click',function(){
+        
+        $('body').css('opacity', '1');
+        $('#showbig').hide();
+        
+    });
+    $('.allBtn').on('click',function(){
+        
+        $('.allItems').show();
+        $('.p-item').css('border', '10px solid rgba(0,0,0,0.9)');
+        
+    });
+    $('.htmlCssBtn').click(function(){
+        
+        $('.allItems').hide();
+        $('.htmlCss').show();
+        $('.p-item').css('border', '10px solid #EC662B');
+        
+    });
+    $('.jsBtn').click(function(){
+        
+        $('.allItems').hide();
+        $('.javascript').show();
+        $('.p-item').css('border', '10px solid #F0DB4F');
+        
+    });
+    $('.jqueryBtn').click(function(){
+        
+        $('.allItems').hide();
+        $('.jquery').show();
+        $('.p-item').css('border', '10px solid #0769AD');
+    });
     
 }
 $(document).ready(main);
